4e024933b27d052887e4af18eea9208cfab2c8ef
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-05 17:04:33 -0500

    Added python file that does the report splitting

---
+++ splitting.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import PyPDF2
+import re
+
+def FindPageNumber(pageObj):
+  text = pageObj.extract_text()
+  lines = text.splitlines()
+  for line in lines:
+    pattern = r"Page\s+(\d+)"
+    match = re.search(pattern, line)
+    if match:
+        page_number = int(match.group(1))
+        return(page_number)
+  return 1
+  
+def FindName(pageObj):
+  text = pageObj.extract_text()
+  lines = text.splitlines()
+  for line in lines:
+    if line.startswith("Student: "):
+      line = line.replace("Student: ", "")
+      line = line.replace(", ", "-")
+      line = line.replace(" ", "_")
+      return line
+def FindDate(pageObj):
+  text = pageObj.extract_text()
+  lines = text.splitlines()
+  for line in lines:
+    for month in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October','November','December']:
+      x = line.find(month)
+      if x!=-1:
+        line = line[x:]
+        line = line.replace(", ", "_")
+        line = line.replace(" ", "_")
+        return line
+
+def WriteReport(filename, writer):
+    newFile = open(filename, 'wb')
+    writer.write(newFile)
+    newFile.close()
+
+def SplitReport(reportname, outdir):
+  # creating a pdf file object
+  pdfFileObj = open(reportname, 'rb')
+    
+  # creating a pdf reader object
+  pdfReader = PyPDF2.PdfReader(pdfFileObj)
+  
+  #outdir = 'doublepagereports/'
+  prevpageObj = pdfReader.pages[0]
+  prevname = FindName(prevpageObj)
+  prevdate = FindDate(prevpageObj)
+  prevfilename = f"{prevname}-{prevdate}.pdf"
+  pdfWriter = PyPDF2.PdfWriter()
+  pdfWriter.add_page(prevpageObj)
+  
+  for i in range(1, len(pdfReader.pages)): 
+    # creating a page object
+    pageObj = pdfReader.pages[i]
+    name = FindName(pageObj)
+    date = FindDate(pageObj)
+    filename = f"{name}-{date}.pdf"
+    if FindPageNumber(pageObj)==1:
+      newPrevFileName = outdir+prevfilename
+      WriteReport(newPrevFileName, pdfWriter)
+      pdfWriter = PyPDF2.PdfWriter()
+      pdfWriter.add_page(pageObj)
+      prevfilename = filename
+      if i==len(pdfReader.pages)-1:
+        newPrevFileName = outdir+prevfilename
+        WriteReport(newPrevFileName, pdfWriter)
+        pdfWriter = PyPDF2.PdfWriter()
+        prevfilename = filename
+    elif i==len(pdfReader.pages)-1:
+      pdfWriter.add_page(pageObj)
+      newPrevFileName = outdir+prevfilename
+      WriteReport(newPrevFileName, pdfWriter)
+      prevfilename = filename
+    else:
+      pdfWriter.add_page(pageObj)
+  
+  # closing the pdf file object
+  pdfFileObj.close()
+  
+SplitReport('report883.pdf', 'doublepagereports/')
