afb93aae51027249c98e647b61a01fd56116df51
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-06 13:57:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ st.write("## Split PDF with progress reports for a whole class into individual P
 st.sidebar.write("## Upload and download")
 mainreport = st.sidebar.file_uploader("Upload a report", type=["pdf"])
 
-st.sidebar.button(label="Split Report", on_click=splitter.SplitReport, args=(mainreport,'alreadyOpened=True'))
+st.sidebar.button(label="Split Report", on_click=splitter.SplitReport, kwargs=(mainreport, alreadyOpened=True))
 st.sidebar.multiselect(label="Individual Reports", options=splitter.filenamelist, default=splitter.filenamelist)
