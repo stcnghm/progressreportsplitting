574acaece77d77fd126d9d39aed6b0bfb03fd685
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-06 11:45:11 -0500

    Revert "Add pdf viewer"

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from splitting import *
 st.set_page_config(layout="wide", page_title="Aspen Progress Report Splitter")
 st.write("## Split PDF with progress reports for a whole class into individual PDFs for each student")
 
-st.sidebar.write("## Upload the report")
+st.sidebar.write("## Upload and download")
 mainreport = st.sidebar.file_uploader("Upload a report", type=["pdf"])
-
-pdfdisplay = f'<iframe src="{mainreport};base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-st.markdown(pdfdisplay, unsafe_allow_html=True)
