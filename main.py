a1d3219bd6c12b05ac346802b832920dfd4135db
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-06 10:36:00 -0500

    Update to main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import streamlit as st
 from splitting import *
+
+st.set_page_config(layout="wide", page_title="Aspen Progress Report Splitter")
+st.write("## Split PDF with progress reports for a whole class into individual PDFs for each student")
