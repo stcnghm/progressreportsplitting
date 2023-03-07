e0c624c04770d64810dd1c7a0ff293f578e0df11
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-07 13:08:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import splitting
 
 st.set_page_config(layout="wide", page_title="Aspen Progress Report Splitter")
 splitter = splitting.Splitter()
-st.sidebar
-sbcont1 = st.sidebar.container()
-sbcont2 = st.sidebar.container()
 
 def splitreportbuttonfunction(*args):
   splitter.SplitReport(*args)
   sbcont2.download_button("Download",splitter.outdir, splitter.outdir)
   
 st.write("## Split PDF with progress reports for a whole class into individual PDFs for each student")
+st.sidebar.write("## Upload and download")
+sbcont1 = st.sidebar.container()
+sbcont2 = st.sidebar.container()
 
-sbcont1.write("## Upload and download")
 mainreport = sbcont1.file_uploader("Upload a report", type=["pdf"])
 
 outdir = sbcont1.text_input("Enter a name to be used as the folder name to download", value='reports')
