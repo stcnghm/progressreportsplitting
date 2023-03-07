ba6f49cd75c2c68f4b62c0cc80894ab302213601
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-07 11:59:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ st.write("## Split PDF with progress reports for a whole class into individual P
 st.sidebar.write("## Upload and download")
 mainreport = st.sidebar.file_uploader("Upload a report", type=["pdf"])
 
+st.sidebar.text_input("Enter a name to be used as the folder name to download", value='reports')
 st.sidebar.button(label="Split Report", on_click=splitter.SplitReport, args=(mainreport, True))
-if mainreport:
-  st.sidebar.download_button(label='Download', data=mainreport, mime='application/pdf')
-filenamelist = splitter.filenamelist
-for i in range(len(filenamelist)):
-    filenamelist[i] = filenamelist[i].split('/')[-1]
-st.sidebar.multiselect(label="Individual Reports", options=filenamelist, default=filenamelist)
+# if mainreport:
+#   st.sidebar.download_button(label='Download', data=mainreport, mime='application/pdf')
+# filenamelist = splitter.filenamelist
+# for i in range(len(filenamelist)):
+#     filenamelist[i] = filenamelist[i].split('/')[-1]
+# st.sidebar.multiselect(label="Individual Reports", options=filenamelist, default=filenamelist)
