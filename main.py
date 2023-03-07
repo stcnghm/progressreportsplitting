f663fe2fdc9f1458f83ed10c9aa398076f3320e4
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-07 13:16:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ splitter = splitting.Splitter()
 st.write("## Split PDF with progress reports for a whole class into individual PDFs for each student")
 st.sidebar.write("## Upload and download")
 sbcont1 = st.sidebar.container()
-sbcont2 = st.sidebar.empty()
+sbcont2 = st.sidebar.container()
+sbcont2.write(" ")
 
 def splitreportbuttonfunction(*args):
   splitter.SplitReport(*args)
