a43f7f4b791c7bc6b6b3db230f1faab1d90ecebb
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-07 12:39:16 -0500

    Add containers in sidebar

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import splitting
 
 
 splitter = splitting.Splitter()
+sbcont1 = st.sidebar.container()
+sbcont2 = st.sidebar.container()
+
 
 def splitreportbuttonfunction(*args):
   splitter.SplitReport(*args)
-  st.sidebar.download_button("Download",outdir)
+  sbcont2.download_button("Download",splitter.outdir)
   
 st.set_page_config(layout="wide", page_title="Aspen Progress Report Splitter")
 st.write("## Split PDF with progress reports for a whole class into individual PDFs for each student")
 
-st.sidebar.write("## Upload and download")
-mainreport = st.sidebar.file_uploader("Upload a report", type=["pdf"])
+sbcont1.write("## Upload and download")
+mainreport = sbcont1.file_uploader("Upload a report", type=["pdf"])
 
-outdir = st.sidebar.text_input("Enter a name to be used as the folder name to download", value='reports')
-st.sidebar.button(label="Split Report", on_click=splitreportbuttonfunction, args=(mainreport, outdir, True))
+outdir = sbcont1.text_input("Enter a name to be used as the folder name to download", value='reports')
+sbcont1.button(label="Split Report", on_click=splitreportbuttonfunction, args=(mainreport, outdir, True))
