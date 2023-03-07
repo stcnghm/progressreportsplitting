7c57c5b8e7af7448cef13505f95bfb98c2d35348
stcnghm <126985250+stcnghm@users.noreply.github.com>
2023-03-07 06:30:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ if mainreport:
 filenamelist = splitter.filenamelist
 for i in range(len(filenamelist)):
     filenamelist[i] = filenamelist[i].split('/')[-1]
+print(f'Filenames: {filenamelist}')
 st.sidebar.multiselect(label="Individual Reports", options=filenamelist, default=filenamelist)
